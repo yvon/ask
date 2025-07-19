ab14504c4aa142b8b9b1cda70c5feadb5c86bdc8
Yvon <git.esophagus963@passinbox.com>
2025-07-19 09:54:44 +0200

    remove pager

--- src/ask.zig
+++ src/ask.zig
@@ -4,18 +4,6 @@ const prompt = @import("prompt.zig");
 const api = @import("api.zig");
 const streaming = @import("streaming.zig");
 
-const Output = struct {
-    pager: std.process.Child,
-
-    pub fn append(self: *Output, data: []const u8) !void {
-        try self.pager.stdin.?.writeAll(data);
-    }
-
-    pub fn close(self: *Output) !void {
-        try closeAndWait(&self.pager);
-    }
-};
-
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -38,52 +26,18 @@ pub fn main() !void {
         std.process.exit(1);
     };
 
-    var output = Output{ .pager = try spawnPager(allocator) };
-
     // Request LLM
     var response = try api.makeRequest(allocator, request);
     var stream = try streaming.Iterator.init(allocator, &response);
 
     // Output
     if (config.prefill) |prefill| {
-        try output.append(prefill);
+        try std.io.getStdOut().writeAll(prefill);
     }
 
     while (try stream.next()) |data| {
-        try output.append(data);
-    }
-    try output.append("\n");
-    try output.close();
-}
-
-fn pagerArgs(allocator: std.mem.Allocator) ![]const []const u8 {
-    const pager = std.process.getEnvVarOwned(allocator, "PAGER") catch "less -XE";
-
-    var args = std.ArrayList([]const u8).init(allocator);
-
-    var token_iter = std.mem.tokenizeScalar(u8, pager, ' ');
-    while (token_iter.next()) |token| {
-        try args.append(token);
+        try std.io.getStdOut().writeAll(data);
     }
 
-    return args.toOwnedSlice();
-}
-
-fn spawnPager(allocator: std.mem.Allocator) !std.process.Child {
-    const argv = try pagerArgs(allocator);
-    return spawnProcess(allocator, argv);
-}
-
-fn closeAndWait(process: *std.process.Child) !void {
-    process.stdin.?.close();
-    process.stdin = null; // or else wait() will try to close it too
-    _ = try process.wait();
-}
-
-fn spawnProcess(allocator: std.mem.Allocator, args: []const []const u8) !std.process.Child {
-    var process = std.process.Child.init(args, allocator);
-    process.stdin_behavior = .Pipe;
-
-    try process.spawn();
-    return process;
+    try std.io.getStdOut().writeAll("\n");
 }
